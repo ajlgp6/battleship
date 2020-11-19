225df00830c606b944765b043ca3e5132bee15f2
Matt Montgomery <33811686+ConfusedPolarBear@users.noreply.github.com>
2020-11-19 14:21:16 -0600

    Add persistence to sound settings
    Also simplified volume button code

--- menu.py
+++ menu.py
@@ -11,12 +11,6 @@ grid_size = 0
 screen = None
 clock = pygame.time.Clock()
 
-#music setup
-pygame.mixer.init()
-pygame.mixer.music.load("assets/sound/mainmenu_bg.mp3")
-pygame.mixer.music.set_endevent(QUIT)
-pygame.mixer.music.play()
-
 # Data for ships that aren't placed
 unplaced = dict()
 allPlaced = False
@@ -26,16 +20,11 @@ client = None
 opponentId = ""
 doGameLoop = True
 
-#set display size
+# Set display size
 WINDOW_SIZE = (900, 620)
-screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
-#get image
-background_image = pygame.image.load("assets/battleship.jpg")
-#scale image
-background_image = pygame.transform.scale(background_image, WINDOW_SIZE)
 
 def draw_text(text, color, surface, x, y):
-	#set font
+	# Set font
 	font = pygame.font.SysFont(None, 30)
 	textobj = font.render(text, 1, color)
 	textrect = textobj.get_rect()
@@ -47,14 +36,13 @@ def soundSettings():
 	click = False
 	timer = 0
 	update = False
-	updating = False
 	while running:
-		pygame.display.set_mode(WINDOW_SIZE,0,32)
-		screen.blit(background_image, (0,0))
-		#get mouse position
+		pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+		screen.blit(background_image, (0, 0))
+		# Get mouse position
 		mx, my = pygame.mouse.get_pos()
 		
-		#creating button
+		# Creating button
 		button_1 = pygame.Rect(50, 100, 200, 50)
 		button_2 = pygame.Rect(50, 200, 200, 50)
 		button_3 = pygame.Rect(50, 300, 200, 50)
@@ -62,39 +50,26 @@ def soundSettings():
 		button_5 = pygame.Rect(302, 100, 200, 50)
 		button_6 = pygame.Rect(302, 200, 200, 50)
 		
-		if button_1.collidepoint((mx, my)):
-			if click:
-				pygame.mixer.music.set_volume(1.0)
-				update = True
-				new = "100%"
-		if button_2.collidepoint((mx, my)):
-			if click:
-				pygame.mixer.music.set_volume(0.75)
-				update = True
-				new = "75%"
-		if button_3.collidepoint((mx, my)):
-			if click:
-				pygame.mixer.music.set_volume(0.5)
-				update = True
-				new = "50%"
-		if button_4.collidepoint((mx, my)):
-			if click:
-				pygame.mixer.music.set_volume(0.25)
-				update = True
-				new = "25%"
-		if button_5.collidepoint((mx, my)):
-			if click:
-				pygame.mixer.music.set_volume(0.0)
-				update = True
-				new = "Muted"
-		if button_6.collidepoint((mx, my)):
-			if click:
+		if click:
+			update = True
+			if button_1.collidepoint((mx, my)):
+				new = saveVolume(1.0)
+			elif button_2.collidepoint((mx, my)):
+				new = saveVolume(0.75)
+			elif button_3.collidepoint((mx, my)):
+				new = saveVolume(0.50)
+			elif button_4.collidepoint((mx, my)):
+				new = saveVolume(0.25)
+			elif button_5.collidepoint((mx, my)):
+				new = saveVolume(0.0)
+			elif button_6.collidepoint((mx, my)):
+				update = False
 				running = False
 		
 		if update:
-			pygame.draw.rect(screen, (0,0,0), (348, 0, 204, 54))
+			pygame.draw.rect(screen, (0, 0, 0), (348, 0, 204, 54))
 			pygame.draw.rect(screen, (25, 130, 227), (350, 2, 200, 50))
-			draw_text("Now "+new, (0,0,0), screen, 450, 27)
+			draw_text(new, (0, 0, 0), screen, 450, 27)
 			timer += 1
 			
 		if (timer == 50):
@@ -102,7 +77,7 @@ def soundSettings():
 			timer = 0
 			update = False
 		
-		#display of button
+		# Display of button
 		pygame.draw.rect(screen, (0, 0, 0), (48, 98, 204, 54))
 		pygame.draw.rect(screen, (25, 70, 227), button_1)
 		pygame.draw.rect(screen, (0, 0, 0), (48, 198, 204, 54))
@@ -116,13 +91,13 @@ def soundSettings():
 		pygame.draw.rect(screen, (0, 0, 0), (300, 198, 204, 54))
 		pygame.draw.rect(screen, (25, 70, 227), button_6)
 
-		#on top because after the display of button
-		draw_text('100%', (0,0,0), screen, 150, 125)
-		draw_text('75%', (0,0,0), screen, 150, 225)
-		draw_text('50%', (0,0,0), screen, 150, 325)
-		draw_text('25%', (0,0,0), screen, 150, 425)
-		draw_text('Mute', (0,0,0), screen, 402, 125)
-		draw_text('Back', (0,0,0), screen, 402, 225)
+		# On top because after the display of button
+		draw_text('100%', (0, 0, 0), screen, 150, 125)
+		draw_text('75%', (0, 0, 0), screen, 150, 225)
+		draw_text('50%', (0, 0, 0), screen, 150, 325)
+		draw_text('25%', (0, 0, 0), screen, 150, 425)
+		draw_text('Mute', (0, 0, 0), screen, 402, 125)
+		draw_text('Back', (0, 0, 0), screen, 402, 225)
 
 		click = False
 		for event in pygame.event.get():
@@ -140,4 +115,49 @@ def soundSettings():
 		pygame.display.update()
 		clock.tick(60)
 		
+# Expects a float in the range [0.0, 1.0]
+def saveVolume(volume):
+	pygame.mixer.music.set_volume(volume)
 
+	f = open(".volume", "w")
+	f.write(str(volume))
+	f.close()
+
+	if volume != 0:
+		# Remove decimal places
+		volume = int(volume * 100)
+		return f"Set volume to {volume}%"
+	else:
+		return "Music muted"
+
+def restoreVolume():
+	raw = "1.0"
+	try:
+		raw = open(".volume", "r").read()
+	except FileNotFoundError:
+		# This must be the first run, ignore
+		pass
+
+	volume = 1.0
+	try:
+		volume = float(raw)
+	except:
+		# Corrupt file, ignore
+		pass
+
+	pygame.mixer.music.set_volume(volume)
+
+def setupMenu():
+	global screen, background_image
+
+	pygame.mixer.init()
+	pygame.mixer.music.load("assets/sound/mainmenu_bg.mp3")
+	pygame.mixer.music.set_endevent(QUIT)
+	pygame.mixer.music.play()
+	restoreVolume()
+
+	screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+	background_image = pygame.image.load("assets/battleship.jpg")
+	background_image = pygame.transform.scale(background_image, WINDOW_SIZE)
+
+setupMenu()
